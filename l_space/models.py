6ffde328c601883bf61817512c3f474d76cd7aa4
samiranne <samira.nedungadi@gmail.com>
2019-10-27 12:17:22 -0700

    Create OwnedBook model

--- l_space/models.py
+++ l_space/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Book(models.Model):
@@ -7,3 +8,9 @@ class Book(models.Model):
 
     def __str__(self):
         return f"Book '{self.title}'"
+
+
+class OwnedBook(models.Model):
+    book_id = models.ForeignKey(to=Book, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
